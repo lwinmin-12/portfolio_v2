cdfc990f0dcf92f753479801b3eedcb14090b839
lmo <lwinminoo@lmoAir.local>
2025-10-27 22:47:56 +0630

    update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,29 @@ export const metadata: Metadata = {
   icons: {
     icon: "/lmo.svg",
   },
+  openGraph: {
+    title: "Lwin Min Oo - Web Developer Portfolio",
+    description:
+      "Showcasing the projects and skills of Lwin Min Oo, a proficient web developer.",
+    siteName: "Lwin Min Oo Portfolio",
+    images: [
+      {
+        url: "/lmo.svg",
+        width: 1200,
+        height: 630,
+        alt: "Lwin Min Oo Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Lwin Min Oo - Web Developer Portfolio",
+    description:
+      "Showcasing the projects and skills of Lwin Min Oo, a proficient web developer.",
+    images: ["/lmo.svg"],
+  },
 };
 
 export default function RootLayout({
